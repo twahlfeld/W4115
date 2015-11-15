8f3cf9a2d59e64862d2a579e8d39369e1c140578
Matthew Haigh <mlh2196@columbia.edu>
2015-11-15 17:02:58 -0500

    Added functions to opcode.ml

--- source/ast.ml
+++ source/ast.ml
@@ -1,5 +1,4 @@
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
-type arg = Lit of int | Reg of string
 
 type expr =
     Literal of int
--- source/opcode.ml
+++ source/opcode.ml
@@ -1,22 +1,24 @@
 type bstmt =
-  | Lit of int             (* Integer Literal *)
-  | Str of string          (* String Literal *)
-  (*| Flit of float        (* Float Literal *)*)
-  | Bin of Ast.op          (* Binary Operators *)
-  (*| Unop of Ast.unop     (* Unary Operators *)*)
-  | Mov of Ast.arg*Ast.arg (* Mov instruction *)
-  | Local_var of int       (* Local Variables, Relative Frame Pointer offset *)
-  | Glob_var of string     (* Global Variables, by absolute label *)
-  | Get_gvar of string     (* Gets Global Variables *)
-  | Set_gvar of string     (* Sets Global Variables  *)
-  | Call of Ast.arg        (* Call function by name or address *)
-  | Push of Ast.arg        (* Pushes arg onto stack *)
-  | Pop of string          (* Pop statck in register *)
-  | Ret                    (* Returns to prev stack frame *)
-  | Fdecl of string        (* Function Declaration *)
-  | Imprt of string        (* Import/Extern function *)
-  | Prologue               (* Start of every stack frame *)
-  | Epilogue               (* End of every stack frame *)
+  | Lit of int                (* Integer Literal *)
+  | Str of string             (* String Literal *)
+  | Bin of Ast.op             (* Binary Operators *)
+  (*| Unop of Ast.unop        (* Unary Operators *)*)
+  | Mov of string * string    (* Mov instruction *)
+  | Local_var of int          (* Local Variables, Relative Frame Pointer offset *)
+  | Glob_var of string        (* Global Variables, by absolute label *)
+  | Get_gvar of string        (* Gets Global Variables *)
+  | Set_gvar of string        (* Sets Global Variables  *)
+  | Call of string            (* Call function by name or address *)
+  | Push of string            (* Pushes arg onto stack *)
+  | Pop of string             (* Pop statck in register *)
+  | Fdecl of string           (* Function Declaration *)
+  | Imprt                     (* Import/Extern function *)
+  | Prologue of string        (* Start of every stack frame *)
+  | Epilogue                  (* End of every stack frame *)
+  | Assign of string * string (* Set variable *)
+  | Ld_var of string          (* Load variable *)
+  | Ld_reg of string          (* Load register into id *)
+  | Ld_lit of int             (* Load lit into register *)
 
 ;;
 
@@ -38,40 +40,42 @@ let rec define_global acc = function
 ;;
 
 let string_of_stmt acc = function
-  | Lit(x)           -> string_of_int x
-  | Str(x)           -> Bytes.to_string 
+  | Lit(x)            -> string_of_int x
+  | Str(x)            -> Bytes.to_string 
                        ("STRING" ^ (string_of_int acc) ^ ":" ^ 
                        (define_global 0 (explode x)))
-  | Bin(Ast.Add)     -> Bytes.to_string "\tadd\teax, edx\n"
-  | Bin(Ast.Sub)     -> Bytes.to_string "\tsub\teax, edx\n"
-  | Bin(Ast.Mult)    -> Bytes.to_string "\tmul\tedx\n"
-  | Bin(Ast.Div)     -> Bytes.to_string "\tdiv\tecx\n"
-  | Bin(Ast.Equal)   -> Bytes.to_string "\tsub\teax, edx\n"
-  | Bin(Ast.Neq)     -> Bytes.to_string "\txor\teax, edx\n"
-  | Bin(Ast.Less)    -> Bytes.to_string "\tsub\teax, edx\n\tshr\teax, 31\n"
-  | Bin(Ast.Leq)     -> Bytes.to_string (
-                     "\tcmp\teax, edx\n"
-                     "\tjle LE" ^ (string_of_int acc) ^ "\n" ^ 
-                      "\tmov\teax, 0\n" ^
-                      "\tjmp\tENDLE" ^ (string_of_int acc) ^"\n" ^ 
-                      "LE" ^ (string_of_int acc) ^ ":\n" ^
-                      "\tmov eax, 1\n" ^
-                      "ENDLE" ^ (string_of_int acc) ^ ":\n")
-  | Bin(Ast.Greater) -> Bytes.to_string (
-                     "\tcmp\teax, edx\n"
-                     "\tjge GE" ^ (string_of_int acc) ^ "\n" ^ 
-                      "\tmov\teax, 0\n" ^
-                      "\tjmp\tENDGE" ^ (string_of_int acc) ^"\n" ^ 
-                      "GE" ^ (string_of_int acc) ^ ":\n" ^
-                      "\tmov eax, 1\n" ^
-                      "ENDGE" ^ (string_of_int acc) ^ ":\n")
-  | Mov(dst, src)    -> Printf.sprintf "\tmov\t%s, %s\n" dst src
-  | Prologue         -> Bytes.to_string "\tpush\tebp\n\tmov\tebp, esp\n"
-  | Epilogue         -> Bytes.to_string "\tpop\tebp\n\tret\n"
-  | Local_var(x)     -> Printf.sprintf "[ebp-%xH]" (x*4)
-  | Glob_var(s)      -> s
-  | Set_gvar(s)      -> Printf.sprintf "\tmov\t[%s], eax\n" s
-  | Get_gvar(s)      -> Pritnf.sprintf "\tmov\teax, [%s]\n" s
+  | Bin(Ast.Add)      -> Bytes.to_string "\tadd\trax, rdx\n"
+  | Bin(Ast.Sub)      -> Bytes.to_string "\tsub\trax, rdx\n"
+  | Bin(Ast.Mult)     -> Bytes.to_string "\tmul\trdx\n"
+  | Bin(Ast.Div)      -> Bytes.to_string "\tdiv\trcx\n"
+  | Bin(Ast.Equal)    -> Bytes.to_string "\tsub\trax, rdx\n"
+  | Bin(Ast.Neq)      -> Bytes.to_string "\txor\trax, rdx\n"
+  | Bin(Ast.Less)     -> Bytes.to_string "\tsub\trax, rdx\n\tshr\trax, 63\n"
+  | Bin(Ast.Leq)      -> Printf.sprintf "\tcmp\trax, rdx\n" ^
+                     "\tsetle dl" ^
+                     "\tmovzx\trax, dl\n"
+  | Bin(Ast.Greater)  -> Printf.sprintf "\tcmp\trax, rdx\n" ^
+                     "\tsetg dl" ^
+                     "\tmovzx\trax, dl\n"
+  | Bin(Ast.Geq)      -> Printf.sprintf "\tcmp\trax, rdx\n" ^
+                     "\tsetge dl" ^
+                     "\tmovzx\trax, dl\n"
+  | Mov(dst, src)     -> Printf.sprintf "\tmov\t%s, %s\n" dst src
+  | Prologue(s)       -> Printf.sprintf "%s\n\tpush\tebp\n\tmov\tebp, esp\n" s
+  | Epilogue          -> Bytes.to_string "\tpop\tebp\n\tret\n"
+  | Local_var(x)      -> Printf.sprintf "[ebp-%xH]" (x*4)
+  | Glob_var(s)       -> s
+  | Set_gvar(s)       -> Printf.sprintf "\tmov\t[%s], rax\n" s
+  | Get_gvar(s)       -> Printf.sprintf "\tmov\trax, [%s]\n" s
+  | Call(s)           -> Printf.sprintf "\tcall\t%s\n" s
+  | Push(s)           -> Printf.sprintf "\tpush\t%s\n" s
+  | Pop(s)            -> Printf.sprintf "\tpop\t%s\n" s
+  | Fdecl(s)          -> Printf.sprintf "%s\n" s
+  | Imprt             -> Printf.sprintf "extern fprintf\nextern fopen\n"
+  | Assign(dest, src) -> Printf.sprintf "\tmov\trax, [%s]\tmov\t[%s], rax\n" src dest
+  | Ld_var(var)       -> Printf.sprintf "\tmov\trdx, rax\nmov\t[%s], rax\n" var
+  | Ld_reg(reg)       -> Printf.sprintf "\tmov\t%s, rax\n" reg
+  | Ld_lit(lit)       -> Printf.sprintf "\tmov\trax, %s\n" (string_of_int lit)
 
->>>>>>> 2a47ccadbb38020cc7fd0b3256e0eb0fe3cf00a2
+
 ;;
