a013fef438f743bf6abfe765bb5c009d52a7fbc1
Ubuntu <ubuntu@ip-172-31-59-102.ec2.internal>
2015-11-23 05:03:15 +0000

    added more tests

--- source/tests/fn_test.sh
+++ source/tests/fn_test.sh
@@ -1,11 +1,11 @@
 TED="../ted"
 NEXT="echo Press enter to continue"
 PROMPT="read answer"
-FN="fn() { int i; } main() { }"
-CAL_FN="fn() { int i; } main() { fn(); }"
-FN_ARG="fn(a) { int i; } main() { fn(1); }"
-FN_ARG_ASGN="fn(a) { int i; i = a; } main() { fn(1); }"
-PR_FN="pr(a) { int s = a; print(stdout, \"hello world\"); } main() { pr(a); }"
+FN="int fn() { int i; } int main() { }"
+CAL_FN="int fn() { int i; } int main() { fn(); }"
+FN_ARG="int fn(int a) { int i; } int main() { fn(1); }"
+FN_ARG_ASGN="int fn(int a) { int i; i = a; } int main() { fn(1); }"
+PR_FN="int pr(int a) { int s = a; print(stdout, \"hello world\"); } int main() { int a; pr(a); }"
 
 printf "Test1: $FN\n"
 echo "$FN" | $TED
