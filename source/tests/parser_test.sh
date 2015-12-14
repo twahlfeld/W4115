ad9b9a010f96ef43120fee1a52b9c6a6877cdd1c
Ubuntu <ubuntu@ip-172-31-59-102.ec2.internal>
2015-12-14 02:58:14 +0000

    added c test example 1

--- source/tests/parser_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MENHIR="menhir --interpret --interpret-show-cst ../parser.mly"
-
-STRING_FUN="string sfun(string s) { return s; }"
-
-printf "Test1:" $STRING_FUN && echo $STRING_FUN | $MENHIR
