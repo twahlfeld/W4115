143d68e3b93e0fad1eba8c6734887a3ae5a19c7d
Gideon Mendels <gideonm@gmail.com>
2015-12-20 18:00:38 -0500

    attr

--- libparse/main.c
+++ libparse/main.c
@@ -2,13 +2,18 @@
 #include "parse.h"
 
 int main(int argc, char *argv[]) {
-    Page *page = fetch("http://www.google.com");
+    Page *page = fetch("http://www.cs.columbia.edu/~sedwards/");
     printf("%s\n", page->url);
     printf("%s\n", page->html);
 
     Element **elm_list = find(page, "img");
     printf("%s\n", elm_list[0]->html);
     printf("%s\n", elm_list[0]->path);
+
+
+    char * attr = NULL;
+    attr = elementAttr(elm_list[0], "alt");
+
     printf("%s\n", elm_list[1]->html);
     printf("%s\n", elm_list[1]->path);
     return 0;
