ce851c3bcb9d552b9c780da906fe7c43f29e6fa8
Ted Ahlfeld <twa2108@columbia.edu>
2015-12-20 21:10:43 -0500

    List implementation

--- src/ted.ml
+++ src/ted.ml
@@ -58,7 +58,19 @@ let _ =
        body    = [];
       }::{
        ftype   = Any;
-       fname   = "listadd";
+       fname   = "listaddlast";
+       formals = Arg(List, "list")::Arg(Any, "data")::[];
+       locals  = [];
+       body    = [];
+      }::{
+       ftype   = Any;
+       fname   = "listaddafter";
+       formals = Arg(List, "list")::Arg(Any, "data")::[];
+       locals  = [];
+       body    = [];
+      }::{
+       ftype   = Any;
+       fname   = "listset";
        formals = Arg(List, "list")::Arg(Any, "data")::[];
        locals  = [];
        body    = [];
@@ -75,9 +87,9 @@ let _ =
        locals  = [];
        body    = [];
       }::{
-       ftype   = Int;
-       fname   = "listsize";
-       formals = Arg(List, "list")::[];
+       ftype   = List;
+       fname   = "listcat";
+       formals = Arg(List, "list1")::Arg(List, "list2")::[];
        locals  = [];
        body    = []
       }::{
