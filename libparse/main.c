329386ab18fa3b6e39218d40bb0b4da281f38278
Gideon Mendels <gideonm@gmail.com>
2015-12-20 19:31:38 -0500

    finished element and page functions

--- libparse/main.c
+++ libparse/main.c
@@ -2,19 +2,22 @@
 #include "parse.h"
 
 int main(int argc, char *argv[]) {
+
+
+
     Page *page = fetch("http://www.cs.columbia.edu/~sedwards/");
-    printf("%s\n", page->url);
-    printf("%s\n", page->html);
+//    printf("%s\n", page->url);
+//    printf("%s\n", page->html);
 
-    Element **elm_list = find(page, "img");
-    printf("%s\n", elm_list[0]->html);
-    printf("%s\n", elm_list[0]->path);
+    Element **elm_list = find(page, "#titlebar");
+    printf("id:  %s\n",elementAttr(elm_list[0],"id"));
+    printf("text:  %s\n",elementText(elm_list[0]));
+    printf("type:  %s\n",elementType(elm_list[0]));
 
+    Element **child_list = elementChildren(page, elm_list[0]);
+    printf("id first child:  %s\n",elementAttr(child_list[1],"id"));
+    printf("text first child:  %s\n",elementText(child_list[1]));
+    printf("type first child:  %s\n",elementType(child_list[1]));
 
-    char * attr = NULL;
-    attr = elementAttr(elm_list[0], "alt");
-
-    printf("%s\n", elm_list[1]->html);
-    printf("%s\n", elm_list[1]->path);
     return 0;
 }
