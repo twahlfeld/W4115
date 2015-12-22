ae91a45304491d548854badbc2db6f3779ea6ba1
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-12-22 18:59:54 -0500

    Adding a few more phantom dependency installations.

--- src/setup.sh
+++ src/setup.sh
@@ -4,3 +4,9 @@ sudo apt-get install phantomjs nodejs nodejs-legacy npm
 sudo npm install phantom -g
 sudo npm install phantom
 sudo apt-get install opam gcc nasm
+
+cd ../tests/
+sudo npm install phantom
+cd ../libparse/Test/
+sudo npm install phantom
+cd ../../src/
